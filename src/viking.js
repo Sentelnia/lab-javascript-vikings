dc3595f86c282f5a702c28969d3f1dc89cf12f68
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-03-04 19:59:03 +0100

    War , le début

--- src/viking.js
+++ src/viking.js
@@ -31,7 +31,33 @@ class Viking extends Soldier{
 }
 
 // Saxon
-class Saxon {}
+class Saxon  extends Soldier{
+    receiveDamage(damage){
+        super.receiveDamage(damage)
+            if (this.health > 0){
+                return `A Saxon has received ${damage} points of damage`
+            }
+            return `A Saxon has died in combat`
+    
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking(Viking){
+        this.vikingArmy.push(Viking);
+    }
+
+    addSaxon(Saxon){
+        this.saxonArmy.push(Saxon);
+    }
+
+    vikingAttack(){};
+    saxonAttack(){};
+    showStatus(){};
+}
